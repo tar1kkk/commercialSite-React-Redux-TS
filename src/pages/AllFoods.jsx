917b757dee97873bdba470bfc62acefd27e1a5e1
Taras <81424893+tar1kkk@users.noreply.github.com>
2023-02-01 18:24:50 +0200

    all foods created

--- src/pages/AllFoods.jsx
+++ src/pages/AllFoods.jsx
@@ -1,10 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Helmet from '../components/Helmet/Helmet';
+import CommonSection from '../components/UI/common-section/CommonSection';
+
+import { Container, Row, Col } from 'reactstrap';
+
+import products from '../assets/fake-data/products';
+import ProductCard from '../components/UI/product-card/ProductCart';
+
+import '../styles/all-foods.css';
+import ReactPaginate from 'react-paginate';
+import '../styles/pagination.css';
 
 function AllFoods(props) {
-	return (
-		<div>
 
-		</div>
+	const [searchTerm, setSearchTerm] = useState('');
+	const [pageNumber, setPageNumber] = useState(0);
+
+	const searchedProduct = products.filter(item => {
+
+		if (searchTerm === '') {
+			return item;
+		}
+		if (item.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+			return item;
+		}
+	})
+
+	const productPerPage = 12;
+	const visitedPage = pageNumber * productPerPage;
+	const displayPage = searchedProduct.slice(visitedPage, visitedPage + productPerPage);
+
+	const pageCount = Math.ceil(searchedProduct.length / productPerPage);
+
+	const changePage = ({ selected }) => {
+		setPageNumber(selected);
+	}
+	return (
+		<Helmet title="All Foods">
+			<CommonSection title="All Foods" />
+
+			<section>
+				<Container>
+					<Row>
+						<Col lg='6' md='6' sm='6'>
+							<div className="search__widget d-flex align-items-center justify-content-between w-50">
+								<input
+									value={searchTerm}
+									onChange={(e) => setSearchTerm(e.target.value)}
+									type='text' placeholder="I'm looking for..." />
+								<span><i class='ri-search-line'></i></span>
+							</div>
+						</Col>
+						<Col lg='6' md='6' sm='6' className='mb-5'>
+							<div className="sorting__widger text-end">
+								<select>
+									<option>Default</option>
+									<option value='ascending'>Alphabetically, A-Z</option>
+									<option value='descending'>Alphabetically, Z-A</option>
+									<option value='high-price'>High Price</option>
+									<option value='low-price'>Low Price</option>
+								</select>
+							</div>
+						</Col>
+
+						{
+							displayPage
+								.map((item) => (
+									<Col lg='3' md='4' sm='6' xs='6' key={item.id} className="mb-4">
+										<ProductCard item={item} />
+									</Col>
+								))
+						}
+
+						<div>
+							<ReactPaginate
+								pageCount={pageCount}
+								onPageChange={changePage}
+								previousLabel='Prev'
+								nextLabel='Next'
+								containerClassName='paginationBttns'
+							/>
+						</div>
+					</Row>
+				</Container>
+			</section>
+		</Helmet>
 	);
 }
 
--- src/pages/FoodDetails.jsx
+++ src/pages/FoodDetails.jsx
@@ -1,10 +1,98 @@
 import React from 'react';
+import prodcucts from '../assets/fake-data/products';
+import { useParams } from 'react-router-dom';
+import Helmet from '../components/Helmet/Helmet';
+import CommonSection from '../components/UI/common-section/CommonSection';
+import { Container, Row, Col } from 'reactstrap';
+import '../styles/product-details.css';
+import productImg from '../assets/images/product_01.1.jpg';
+import { useState } from 'react';
 
-function FoodDetails(props) {
+function FoodDetails() {
+	const [tab, setTab] = useState('desc');
+	const { id } = useParams();
+
+	const product = prodcucts.find(item => item.id === id);
+
+	const [previewImg, setPreviewImg] = useState(product.image01);
 	return (
-		<div>
+		<Helmet title='Product-details'>
+			<CommonSection title='product 01' />
+			<section>
+				<Container>
+					<Row>
+						<Col lg='2' md='2'>
+							<div className="product__images">
+								<div className="img__item" onClick={() => setPreviewImg(prodcucts.image01)}>
+									<img src={product.image01} alt="" className='w-50' />
+								</div>
+								<div className="img__item" onClick={() => setPreviewImg(prodcucts.image02)}>
+									<img src={product.image02} alt="" className='w-50' />
+								</div>
+								<div className="img__item" onClick={() => setPreviewImg(prodcucts.image03)}>
+									<img src={product.image03} alt="" className='w-50' />
+								</div>
+							</div>
+						</Col>
 
-		</div>
+						<Col lg='4' md='4'>
+							<div className="product__main-img">
+								<img src={previewImg} alt="" className='w-100' />
+							</div>
+						</Col>
+
+						<Col lg='6' md='6'>
+							<div className="single__product-content">
+								<h2 className='product__title mb-3'>{product.title}</h2>
+								<p className='product__price'>Price <span>$ {product.price}</span></p>
+								<p className='category'>Category : <span>{product.category}</span></p>
+
+								<button className='addTOCart__btn'>Add to Cart</button>
+							</div>
+						</Col>
+
+						<Col lg='12'>
+							<div className="tabs d-flex align-items-center gap-5 py-2">
+								<h6 className='tab__active'>Description</h6>
+								<h6>Review</h6>
+							</div>
+
+							<div className="tab__content">
+								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum minima nihil ipsam hic vitae expedita, consequuntur nostrum facilis cumque consectetur maxime fuga veniam inventore. Reiciendis, laboriosam iusto? A est consequuntur error ipsa repudiandae, ullam, voluptatum molestiae ipsam, voluptates eum repellendus.</p>
+							</div>
+
+							<div className="tab__form mb-3">
+								<div className="review">
+									<p className="user__name mb-0">Taras Leuta</p>
+									<p className='user__email'>tarasleuta@gmail.com</p>
+									<p className='feedback__text'>great product</p>
+								</div>
+								<div className="review">
+									<p className="user__name mb-0">Taras Leuta</p>
+									<p className='user__email'>tarasleuta@gmail.com</p>
+									<p className='feedback__text'>great product</p>
+								</div>
+								<form className='form'>
+
+									<div className='form__group'>
+										<input type="text" placeholder='Enter your name' />
+									</div>
+
+									<div className='form__group'>
+										<input type="text" placeholder='Enter your name' />
+									</div>
+
+									<div className='form__group'>
+										<textarea type="text" placeholder='Enter your name' />
+									</div>
+									<button type='submit' className='addTOCart__btn'>Submit</button>
+								</form>
+							</div>
+						</Col>
+					</Row>
+				</Container>
+			</section>
+		</Helmet>
 	);
 }
 
